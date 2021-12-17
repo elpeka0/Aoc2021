e84c80c23c39895d94250eb638556695149a9f0b
Ludwig Pumberger <ludwig.pumberger@gmail.com>
2021-12-17 18:35:11 +0100

    Day 17 reworked

--- Aoc/Aoc/Day17.cs
+++ Aoc/Aoc/Day17.cs
@@ -12,56 +12,6 @@ namespace Aoc
         {
         }
 
-        private IEnumerable<(int X, int Y)> Shoot(int vx, int vy)
-        {
-            var x = 0;
-            var y = 0;
-            while (true)
-            {
-                x += vx;
-                y += vy;
-                if (vx > 0)
-                {
-                    vx -= Math.Sign(x);
-                }
-                --vy;
-                yield return (x, y);
-            }
-        }
-
-        private IEnumerable<(int X, int Y)> AllPairs()
-        {
-            var x = 0;
-            var y = 0;
-            var down = false;
-            while (true)
-            {
-                if (x == 0 && !down)
-                {
-                    ++y;
-                    down = true;
-                }
-                else if (y == 0 && down)
-                {
-                    ++x;
-                    down = false;
-                }
-                else if (down)
-                {
-                    ++x;
-                    --y;
-                }
-                else
-                {
-                    ++y;
-                    --x;
-                }
-
-                yield return (x, y);
-                yield return (x, -y);
-            }
-        }
-
         private (int XLow, int YLow, int XHigh, int YHigh) GetInput()
         {
             var l = GetInputLines(false).First();
@@ -72,55 +22,69 @@ namespace Aoc
             return (int.Parse(xpart[0]), int.Parse(ypart[0]), int.Parse(xpart[1]), int.Parse(ypart[1]));
         }
 
-        private IEnumerable<(int vx, int vy, int localmax)> TexasMode()
-        {
-            var (xlow, ylow, xhigh, yhigh) = this.GetInput();
-            
-            foreach (var (vx, vy) in this.AllPairs())
-            {
-                var localmax = int.MinValue;
-                foreach (var p in this.Shoot(vx, vy))
-                {
-                    if (p.Y > localmax)
-                    {
-                        localmax = p.Y;
-                    }
-
-                    if (p.X >= xlow && p.X <= xhigh && p.Y >= ylow && p.Y <= yhigh)
-                    {
-                        yield return (vx, vy, localmax);
-                        break;
-                    }
-
-                    if (p.Y < ylow)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
         public override void Solve()
         {
-            var maxy = int.MinValue;
-            foreach (var (_, _, localmax) in this.TexasMode())
-            {
-                if (localmax > maxy)
-                {
-                    maxy = localmax;
-                    Console.WriteLine(maxy);
-                }
-            }
+            //var maxy = int.MinValue;
+            //foreach (var (_, _, localmax) in this.TexasMode())
+            //{
+            //    if (localmax > maxy)
+            //    {
+            //        maxy = localmax;
+            //        Console.WriteLine(maxy);
+            //    }
+            //}
         }
+
+        private int Triangle(int n) => n * (n + 1) / 2;
+
+        private int SeedX(int t, int x) => t + (x - Triangle(t)) / t;
+
+        private int SeedY(int t, int y) => (y + Triangle(t - 1)) / t;
+
+        private bool CanHitX(int t, int x) => (x - Triangle(t)) % t == 0;
+
+        private bool CanHitY(int t, int y) => (y - Triangle(t - 1)) % t == 0;
 
         public override void SolveMain()
         {
-            var points = new HashSet<(int, int)>();
-            foreach (var (vx, vy, _) in this.TexasMode())
+            var (xlow, ylow, xhigh, yhigh) = this.GetInput();
+
+            var seen = new HashSet<(int, int)>();
+            for (int x = xlow; x <= xhigh; ++x)
             {
-                points.Add((vx, vy));
-                Console.WriteLine(points.Count);
+                for (int t = 1;Triangle(t) <= x; ++t)
+                {
+                    if (CanHitX(t, x))
+                    {
+                        var vx = SeedX(t, x);
+                        for (int y = ylow; y <= yhigh; ++y)
+                        {
+                            if (CanHitY(t, y))
+                            {
+                                var vy = SeedY(t, y);
+                                seen.Add((vx, vy));
+                            }
+                            if (x == Triangle(t))
+                            {
+                                // Stop on x
+                                for (int s = t + 1;; ++s)
+                                {
+                                    if (CanHitY(s, y))
+                                    {
+                                        var vy = SeedY(s, y);
+                                        seen.Add((vx, vy));
+                                    }
+                                    if (SeedY(s, y) > Math.Abs(y))
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
+            Console.WriteLine(seen.Count);
         }
     }
 }
