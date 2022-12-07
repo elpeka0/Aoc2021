ead873559b7ddad13aa18cc5c7b6f0b586f95596
Ludwig Pumberger <ludwig.pumberger@kontron.com>
2022-12-07 08:38:10 +0100

    6

--- Aoc/Aoc/Program.cs
+++ Aoc/Aoc/Program.cs
@@ -7,7 +7,7 @@ namespace Aoc
     {
         static void Main(string[] args)
         {
-            new Day05().Solve();
+            new Day06().Solve();
         }
     }
 }
---
+++ Aoc/Aoc/y2022/Day06.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.y2022
+{
+    public class Day06 : DayBase
+    {
+
+        public Day06() : base(6)
+        {
+        }
+
+        public override void Solve()
+        {
+            const int Window = 14;
+            var line = GetInputLines(false).First();
+            Console.WriteLine(Enumerable.Range(0, line.Length).First(i => line.Substring(i, Window).Distinct().Count() == Window) + Window);
+        }
+
+        public override void SolveMain()
+        {
+        }
+    }
+}
