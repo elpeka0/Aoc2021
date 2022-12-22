b21f22945d18cdc24c59b1e2c6cb23214a463323
Ludwig Pumberger <ludwig.pumberger@gmail.com>
2022-12-22 23:51:48 +0100

    22-sigh

--- Aoc/Aoc/y2022/Day22.cs
+++ Aoc/Aoc/y2022/Day22.cs
@@ -18,17 +18,18 @@ namespace Aoc.y2022
             (0, -1)
         };
 
-        private record Position(int X, int Y, int Facing, int Face);
+        private record Position(int X, int Y, int Facing);
 
-        private (Grid<char> Grid, List<Instruction> Instruction, Position Position) GetInput()
+        private (Grid<char> Grid, List<Instruction> Instruction, Position Position) GetInput(bool pad)
         {
+            var padding = pad ? new[] { string.Empty } : new string[0];
             var lines = GetInputLines(false);
             var grid = Grid<char>.FromLines(
-                new[] { string.Empty }.Concat(
+                padding.Concat(
                 lines
                 .TakeWhile(l => l != string.Empty)
-                .Select(l => $" {l} ")
-                ).Concat(new[] { string.Empty })
+                .Select(l => pad ? $" {l} " : l)
+                ).Concat(padding)
                 .ToList(), c => c);
             foreach (var idx in grid.Indexes())
             {
@@ -40,7 +41,7 @@ namespace Aoc.y2022
             var instructions = ParseInstructions(lines);
 
             var x = 0;
-            var y = 1;
+            var y = pad ? 1 : 0;
 
             foreach (var idx in grid.Row(y).Indexes())
             {
@@ -50,7 +51,7 @@ namespace Aoc.y2022
                     break;
                 }
             }
-            return (grid, instructions, new Position(x, y, 0, 0));
+            return (grid, instructions, new Position(x, y, 0));
         }
 
         private static List<Instruction> ParseInstructions(IEnumerable<string> lines)
@@ -70,34 +71,6 @@ namespace Aoc.y2022
             }
             instructions.Add(new Instruction(int.Parse(s.Substring(p, i - p)), 0));
             return instructions;
-        }
-
-        private (Dictionary<int, Grid<char>> Cube, List<Instruction> Instruction) GetInput2()
-        {
-            var lines = GetInputLines(false);
-            var grid = Grid<char>.FromLines(
-                lines
-                .TakeWhile(l => l != string.Empty)
-                .ToList(), c => c);
-
-            var sl = 50;
-            var cube = new Dictionary<int, Grid<char>>();
-            var indexLoc = new (int X, int Y)[] { (sl, 0), (2*sl, 0), (sl, sl), (0, 3*sl), (0, 2*sl), (sl, 2*sl) };
-
-            for (int i = 1; i <= 6; ++i)
-            {
-                var sg = new Grid<char>(sl, sl);
-                foreach (var idx in sg.Indexes())
-                {
-                    sg[idx.X, idx.Y] = grid[idx.X + indexLoc[i - 1].X, idx.Y + indexLoc[i - 1].Y];
-                }
-                cube[i] = sg;
-            }
-
-            var instructions = ParseInstructions(lines);
-
-
-            return (cube, instructions);
         }
 
         public Day22() : base(22)
@@ -150,7 +123,7 @@ namespace Aoc.y2022
 
         public override void Solve()
         {
-            var (grid, instructions, position) = GetInput();
+            var (grid, instructions, position) = GetInput(true);
 
             foreach (var instruction in instructions)
             {
@@ -160,51 +133,152 @@ namespace Aoc.y2022
             Console.WriteLine(v);
         }
 
-        private (Position New, bool DidMove) MoveOne2(Position pos, Dictionary<int, Grid<char>> cube)
+        private record Rule(int X, int Y, Vector XM, Vector YM, Vector ZM, Vector A);
+
+        private IEnumerable<Rule> Sample()
         {
-            var f = facing[pos.Facing];
-            var grid = cube[pos.Face];
+            yield return new Rule(2, 0, new Vector(1, 0, 0), new Vector(0, -1, 0), new Vector(0, 0, 0), new Vector(0, 1, 0));
+            yield return new Rule(0, 1, new Vector(-1, 0, 0), new Vector(0, 0, 0), new Vector(0, 1, 0), new Vector(1, 1, 0));
+            yield return new Rule(1, 1, new Vector(0, 0, 0), new Vector(-1, 0, 0), new Vector(0, 1, 0), new Vector(0, 1, 0));
+            yield return new Rule(2, 1, new Vector(1, 0, 0), new Vector(0, 0, 0), new Vector(0, 1, 0), new Vector(0, 0, 0));
+            yield return new Rule(2, 2, new Vector(1, 0, 0), new Vector(0, 1, 0), new Vector(0, 0, 0), new Vector(0, 1, 1));
+            yield return new Rule(3, 2, new Vector(0, 0, 0), new Vector(0, 1, 0), new Vector(-1, 0, 0), new Vector(1, 0, 1));
+        }
+        private IEnumerable<Rule> Actual()
+        {
+            yield break;
+        }
 
-            var newPos = pos with { X = pos.X + f.X, Y = pos.Y + f.Y };
-
-            if (newPos.X < 0)
+        private record CubeCell(char C, List<(int X, int Y)> Coords)
+        {
+            public override string ToString()
             {
-                var (face, facing) = pos.Face switch
+                var c = string.Join(':', Coords.Select(c => c.X.ToString().PadRight(2) + "," + c.Y.ToString().PadRight(2)));
+                c = c.PadRight(17);
+                return $"[{C} {c}]";
+            }
+        }
+
+        private Grid<CubeCell> Cubify(Grid<char> grid)
+        {
+            var len = (int) Math.Sqrt(grid.Count(c => c != ' ') / 6);
+            var res = new Grid<CubeCell>(grid.Width, grid.Height);
+            res.Fill(new CubeCell(' ', new List<(int X, int Y)>()));
+            var rules = len == 4 ? Sample() : Actual();
+
+            var map = new Dictionary<(int, int, int), List<(int, int)>>();
+
+            foreach (var rule in rules)
+            {
+                var px = rule.X * len;
+                var py = rule.Y * len;
+                for (var x = 0; x < len; x++)
                 {
-                    1 => (4, 3),
-                    2 => (4, 2),
-                    3 => (1, 3),
-                    4 => (6, 3),
-                    5 => (5, 0),
-                    6 => (3, 3)
-                };
+                    for (var y = 0; y < len; y++)
+                    {
+                        int Mul(Vector m, int a) => x * m.X + y * m.Y + a * (len - 1);
+                        var tx = Mul(rule.XM, rule.A.X);
+                        var ty = Mul(rule.YM, rule.A.Y);
+                        var tz = Mul(rule.ZM, rule.A.Z);
+
+                        var c = grid[px + x, py + y];
+                        if (!map.TryGetValue((tx, ty, tz), out var l))
+                        {
+                            l = new List<(int, int)>();
+                            map[(tx, ty, tz)] = l;
+                        }
+                        l.Add((px + x, py + y));
+                        res[px + x, py + y] = new CubeCell(c, l);
+                    }
+                }
             }
 
+            return res;
+        }
+
+        private bool IsEmpty(int x, int y, Grid<CubeCell> grid) =>
+            x < 0 || x >= grid.Width || y < 0 || y >= grid.Height || grid[x, y].C == ' ';
+
+        private (Position New, bool DidMove) MoveOne2(Position pos, Grid<CubeCell> grid)
+        {
+            var f = facing[pos.Facing];
+            var newPos = pos with { X = pos.X + f.X, Y = pos.Y + f.Y };
+
+            if (IsEmpty(newPos.X, newPos.Y, grid))
+            {
+                var coords = grid[pos.X, pos.Y].Coords;
+                if (coords.Count == 1)
+                {
+                    throw new InvalidOperationException("Fuck!");
+                }
+
+                if (coords.Count == 3)
+                {
+                    throw new InvalidOperationException("Sigh!");
+                }
+
+                var nc = coords.First(x => x != (pos.X, pos.Y));
+                newPos = pos with { X = nc.X, Y = nc.Y };
+
+                int t;
+                for (t = 0; t < facing.Length; ++t)
+                {
+                    if (IsEmpty(newPos.X + facing[t].X, newPos.Y + facing[t].Y, grid))
+                    {
+                        newPos = newPos with { Facing = t switch
+                        {
+                            0 => 2,
+                            1 => 3,
+                            2 => 0,
+                            3 => 1
+                        }};
+                        break;
+                    }
+                }
+            }
 
             var c = grid[newPos.X, newPos.Y];
-            if (c == '#')
+
+            if (c.C == '#')
             {
                 return (pos, false);
             }
-            if (c == '.')
+            if (c.C == '.')
             {
-                return (pos with { X = pos.X + f.X, Y = pos.Y + f.Y }, true);
+                return (newPos, true);
             }
 
             throw new InvalidOperationException("???");
         }
 
+        private Position Move2(Position pos, Instruction instruction, Grid<CubeCell> grid)
+        {
+            var didMove = true;
+            for (int i = 0; i < instruction.Move && didMove; ++i)
+            {
+                (pos, didMove) = MoveOne2(pos, grid);
+            }
+
+            var f = (pos.Facing + instruction.Turn) % facing.Length;
+            if (f < 0)
+            {
+                f += facing.Length;
+            }
+
+            return pos with { Facing = f };
+        }
+
         public override void SolveMain()
         {
-            var (cube, instructions) = GetInput2();
-            var position = new Position(0, 0, 0, 1);
+            var (grid, instructions, position) = GetInput(false);
+            var cube = Cubify(grid);
 
-            foreach (var face in cube)
+            foreach (var instruction in instructions)
             {
-                Console.WriteLine("FACE " + face.Key);
-                Console.WriteLine(face.Value);
-                Console.WriteLine();
+                position = Move2(position, instruction, cube);
             }
+            var v = 1000 * position.Y + 4 * position.X + position.Facing;
+            Console.WriteLine(v);
         }
     }
 }
