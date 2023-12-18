f1e8b6c1aee20da1920f5d186e5b8752f437d626
Ludwig Pumberger <ludwig.pumberger@gmail.com>
2023-12-18 21:11:51 +0100

    18

--- Aoc/Aoc/y2023/Day18.cs
+++ Aoc/Aoc/y2023/Day18.cs
@@ -75,30 +75,31 @@ namespace Aoc.y2023
                 }
             }
 
-            var y = (grid.MaxY + grid.MinY) / 2;
+            Console.WriteLine(grid.Count(c => c != '.'));
+        }
+
+        private void FloodFillArea(Grid<char> grid)
+        {
+            var y = (grid.MaxY + grid.MinY) / 2 + 1;
             var x = grid.MinX - 1;
             var delta = Math.Sign(grid.MaxX - grid.MinX);
-
             while (grid[x, y] != '#')
             {
                 x += delta;
             }
 
             x++;
-            
+
             var filled = Utils.FloodFill(new Vector(x, y), p => grid.Neighbors(p, false).Where(i => grid[i] == '.'));
             foreach (var c in filled)
             {
                 grid[c] = 'O';
             }
-
-            File.WriteAllText("C:\\dev\\fuck.txt", grid.ToString());
-            Console.WriteLine(grid.Count(c => c != '.'));
         }
 
         public override void SolveMain()
         {
-            var instructions = GetInputLines(false).Select(Parse).ToList();
+            var instructions = GetInputLines(false).Select(ParseMain).ToList();
             var p = Vector.Origin;
             var loop = new List<Vector>();
             foreach (var i in instructions)
@@ -109,6 +110,66 @@ namespace Aoc.y2023
             loop.Add(p);
 
             Console.WriteLine(Area(loop));
+            /*var loop = new List<Vector>
+            {
+                new Vector(6, 5) * 2,
+                new Vector(2, 5) * 2,
+                new Vector(2, 3) * 2,
+                new Vector(3, 3) * 2,
+                new Vector(3, 2) * 2,
+                new Vector(1, 2) * 2,
+                new Vector(1, 5) * 2,
+                new Vector(0, 5) * 2,
+                new Vector(0, 8) * 2,
+                new Vector(1, 8) * 2,
+                new Vector(1, 7) * 2,
+                new Vector(2, 7) * 2,
+                new Vector(2, 10) * 2,
+                new Vector(6, 10) * 2,
+                new Vector(6, 9) * 2,
+                new Vector(4, 9) * 2,
+                new Vector(4, 7) * 2,
+                new Vector(5, 7) * 2,
+                new Vector(5, 8) * 2,
+                new Vector(7, 8) * 2,
+                new Vector(7, 3) * 2,
+                new Vector(5, 3) * 2,
+                new Vector(5, 0) * 2,
+                new Vector(3, 0) * 2,
+                new Vector(3, 1) * 2,
+                new Vector(4, 1) * 2,
+                new Vector(4, 4) * 2,
+                new Vector(6, 4) * 2,
+                new Vector(6, 5) * 2
+            };
+            var grid = Gridify(loop, true);
+            FloodFillArea(grid);
+            Console.WriteLine(grid);
+            Console.WriteLine(grid.Count(c => c != '.'));
+            Console.WriteLine(Area(loop));*/
+        }
+
+        private Grid<char> Gridify(List<Vector> loop, bool close)
+        {
+            var res = Grid<char>.Dynamic('.');
+            for (var i = 0; i < (close ? loop.Count : loop.Count - 1); i++)
+            {
+                var n = loop[i + 1 < loop.Count ? i + 1 : 0];
+                var delta = n - loop[i];
+                var dx = Math.Sign(delta.X);
+                var dy = Math.Sign(delta.Y);
+                Vector p;
+                for (p = loop[i]; p != new Vector(n.X, loop[i].Y);p += new Vector(dx, 0))
+                {
+                    res[p] = '#';
+                }
+                for (; p != n; p += new Vector(0, dy))
+                {
+                    res[p] = '#';
+                }
+                res[p] = '#';
+            }
+            return res;
         }
 
         private long Circumference(List<Vector> path)
@@ -117,7 +178,7 @@ namespace Aoc.y2023
             for (var i = 0; i < path.Count; ++i)
             {
                 var n = path[i + 1 < path.Count ? i + 1 : 0];
-                res += Math.Abs(n.X - path[i].X) + Math.Abs(n.Y - path[i].Y);
+                res += Distance(path[i], n);
             }
             return res;
         }
@@ -129,6 +190,7 @@ namespace Aoc.y2023
 
         private long Area(List<Vector> path)
         {
+            // Console.WriteLine(Gridify(path, false));
             var xMin = long.MaxValue;
             var xMax = long.MinValue;
             var yMin = long.MaxValue;
@@ -142,38 +204,55 @@ namespace Aoc.y2023
                 if(v.Y > yMax) yMax = v.Y;
             }
 
+            var start = 0;
+            while (path[start].X != xMin
+                && path[start].Y != yMin
+                && path[start].X < xMax
+                && path[start].Y < yMax)
+            {
+                start++;
+            }
+
             var sub = new List<Vector>();
             var neg = 0L;
-            foreach (var p in path)
+            for(var i = 0;i < path.Count;i++)
             {
+                var p = path[(start + i) % path.Count];
                 var xBounded = p.X == xMin || p.X == xMax;
                 var yBounded = p.Y == yMin || p.Y == yMax;
 
                 if (xBounded != yBounded)
                 {
-                    if (sub.Count > 0)
+                    if (sub.Count > 1)
                     {
                         sub.Add(p);
-                        neg += Area(sub) - Circumference(sub) + Distance(sub[0], p) - 1;
-                        sub.Clear();
+                        neg += NegativeArea(sub);
                     }
-                    else
-                    {
-                        sub.Add(p);
-                    }
+                    sub.Clear();
                 }
-                else if (!xBounded)
+                else if (xBounded)
                 {
-                    sub.Add(p);
+                    sub.Clear();
                 }
+                sub.Add(p);
             }
 
-            if (sub.Count > 0)
+            if (sub.Count > 1)
             {
-                neg += Area(sub) - Circumference(sub) + Distance(sub[0], path.Last()) - 1;
+                var end = path[(start + path.Count - 1) % path.Count];
+                sub.Add(end);
+                sub.Add(path[start]);
+                neg += NegativeArea(sub);
             }
 
-            return (xMax - xMin + 1)*(yMax - yMin + 1) - neg;
+            var res = (xMax - xMin + 1)*(yMax - yMin + 1) - neg;
+            // Console.WriteLine($"Area: {res} Neg: {res - Circumference(path) + Distance(path.First(), path.Last()) - 1}");
+            return res;
+        }
+
+        private long NegativeArea(List<Vector> path)
+        {
+            return Area(path) - Circumference(path) + Distance(path.First(), path.Last()) - 1;
         }
     }
 }
