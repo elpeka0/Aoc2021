9076e87c3e46f8e3f11e62f975dd604f52d090d3
Ludwig Pumberger <ludwig.pumberger@gmail.com>
2023-12-23 11:37:58 +0100

    23

--- Aoc/Aoc/y2023/Day23.cs
+++ Aoc/Aoc/y2023/Day23.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +18,7 @@ namespace Aoc.y2023
         {
             var grid = Grid<char>.FromLines(GetInputLines(false).ToList(), c => c);
             var start = grid.Row(0).Indexes().First(i => grid[i] == '.');
-            var target = grid.Row(grid.Width - 1).Indexes().First(i => grid[i] == '.');
+            var target = grid.Row(grid.Height - 1).Indexes().First(i => grid[i] == '.');
 
             var res = LongestPath(start, target, grid, CanMove);
             Console.WriteLine(res);
@@ -41,81 +40,126 @@ namespace Aoc.y2023
         {
             var grid = Grid<char>.FromLines(GetInputLines(false).ToList(), c => c);
             var start = grid.Row(0).Indexes().First(i => grid[i] == '.');
-            var target = grid.Row(grid.Width - 1).Indexes().First(i => grid[i] == '.');
+            var target = grid.Row(grid.Height - 1).Indexes().First(i => grid[i] == '.');
 
             var res = LongestPath(start, target, grid, p => grid.Neighbors(p, false));
             Console.WriteLine(res);
         }
 
-        private class State
+        private record TreeLink(int Length, TreeNode Child)
         {
-            public Vector s;
-            public HashSet<Vector> visited;
-            public IEnumerator<Vector> e;
-            public long best = -1L;
-            public Action<long> continuation;
+            public override string ToString()
+            {
+                return $"{Length} -> {Child}";
+            }
+        }
+
+        private class TreeNode
+        {
+            public List<TreeLink> Links { get; } = new List<TreeLink>();
+
+            public bool IsTarget { get; }
+
+            public Vector Pos { get; }
+
+            public TreeNode(Vector pos, bool isTarget)
+            {
+                Pos = pos;
+                IsTarget = isTarget;
+            }
+
+            public override string ToString()
+            {
+                return $"[{(IsTarget ? "Target " : "")}{Pos}]";
+            }
+
+            public string Visualize()
+            {
+                var sb = new StringBuilder();
+                Utils.FloodFill(this, (n, _) =>
+                {
+                    foreach (var link in n.Links)
+                    {
+                        sb.AppendLine($"{n}->{link.Child}({link.Length})");
+                    }
+                    return n.Links.Select(l => l.Child);
+                });
+                return sb.ToString();
+            }
+        }
+
+        private TreeNode IntoTree(Vector start, Vector target, Grid<char> grid, Func<Vector, IEnumerable<Vector>> stepFunc)
+        {
+            var lookup = new Dictionary<Vector, TreeNode>();
+            Utils.FloodFill(start, (v, _) =>
+            {
+                lookup[v] = new TreeNode(v, v == target);
+                return stepFunc(v).Where(n => grid[n] != '#');
+            });
+
+            Utils.FloodFill(lookup[start], (n, _) =>
+            {
+                n.Links.AddRange(stepFunc(n.Pos).Where(i => grid[i] != '#').Select(i => new TreeLink(1, lookup[i])));
+                return n.Links.Select(l => l.Child);
+            });
+
+            Utils.FloodFill(lookup[start], (n, _) =>
+            {
+                var r = n.Links.Select(l => Shorten(l, n))
+                    .Where(n => n != null)
+                    .GroupBy(n => n.Child)
+                    .Select(g => g.OrderByDescending(n => n.Length).First())
+                    .ToList();
+                n.Links.Clear();
+                n.Links.AddRange(r);
+                return n.Links.Select(l => l.Child);
+            });
+
+            return lookup[start];
+        }
+
+        TreeLink Shorten(TreeLink link, TreeNode head)
+        {
+            var cost = 1;
+            while (true)
+            {
+                var l = link.Child.Links.Where(n => n.Child != head).ToList();
+                if (l.Count != 1 || l[0].Length > 1)
+                {
+                    return new TreeLink(cost, link.Child);
+                }
+                head = link.Child;
+                link = l[0];
+                cost++;
+            }
         }
 
         private long LongestPath(Vector start, Vector target, Grid<char> grid, Func<Vector, IEnumerable<Vector>> stepFunc)
         {
-            var stack = new Stack<Action>();
-            long res = 0L;
-            var initial = new State
-            {
-                s = start,
-                visited = new HashSet<Vector>(),
-                continuation = l => res = l
-            };
-            stack.Push(() => Head(initial));
-            while (stack.Count > 0)
-            {
-                var next = stack.Pop();
-                next();
-            }
-            return res;
+            var root = IntoTree(start, target, grid, stepFunc);
+            Console.WriteLine(root.Visualize());
+            return Impl(root, new HashSet<TreeNode>());
 
-            void Head(State state)
+            long Impl(TreeNode s, HashSet<TreeNode> visited)
             {
-                state.visited.Add(state.s);
-                if (state.s == target)
+                if (s.IsTarget)
                 {
-                    state.continuation(0L);
-                    return;
+                    return 0;
                 }
-
-                state.e = stepFunc(state.s).Where(i => grid[i] != '#').GetEnumerator();
-
-                stack.Push(() => Tail(state));
-                stack.Push(() => Body(state));
-            }
-
-            void Body(State state)
-            {
-                if (state.e.MoveNext())
+                var best = -1L;
+                foreach (var n in s.Links)
                 {
-                    stack.Push(() => Body(state));
-                    if (!state.visited.Contains(state.e.Current))
+                    if (!visited.Contains(n.Child))
                     {
-                        var ns = new State
+                        visited.Add(s);
+                        var b = Impl(n.Child, visited.ToHashSet());
+                        if (b >= 0 && b + n.Length > best)
                         {
-                            s = state.e.Current,
-                            visited = state.visited.ToHashSet(),
-                            continuation = l =>
-                            {
-                                if (l > state.best)
-                                {
-                                    state.best = l;
-                                }
-                            }
-                        };
-                        stack.Push(() => Head(ns));
+                            best = b + n.Length;
+                        }
                     }
                 }
-            }
-
-            void Tail(State state)
-            {
-                state.continuation(state.best + 1);
+                return best;
             }
         }
     }
