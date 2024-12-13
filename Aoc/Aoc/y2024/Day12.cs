3f27353e537244a805b6bb2de4b0ab49255717d0
Ludwig Pumberger <ludwig.pumberger@kontron.com>
2024-12-13 08:32:43 +0100

    12+13

---
+++ Aoc/Aoc/y2024/Day12.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Geometry;
+
+namespace Aoc.y2024
+{
+    public class Day12 : DayBase
+    {
+        public Day12() : base(12)
+        {
+        }
+
+        public override void Solve()
+        {
+            var grid = Grid<char>.FromLines(this.GetInputLines().ToList(), c => c);
+            var seen = new HashSet<Vector>();
+
+            var total = 0;
+
+            foreach (var index in grid.Indexes())
+            {
+                if (seen.Contains(index))
+                {
+                    continue;
+                }
+
+                var area = 0;
+                var perimeter = 0;
+
+                Utils.FloodFill(index, (n, _) =>
+                {
+                    if (!seen.Add(n))
+                    {
+                        return Enumerable.Empty<Vector>();
+                    }
+
+                    area++;
+                    perimeter += n.Neighbors(false).Count(x => !grid.IsInBounds(x) || grid[x] != grid[n]);
+                    return grid.Neighbors(n, false).Where(x => grid[x] == grid[n]);
+                });
+
+                total += area * perimeter;
+            }
+
+            Console.WriteLine(total);
+        }
+
+        public override void SolveMain()
+        {
+            var original = Grid<char>.FromLines(this.GetInputLines().ToList(), c => c);
+            var grid = Grid<char>.WithSize(original.Width * 3, original.Height * 3);
+            foreach (var index in original.Indexes())
+            {
+                for (var x = 0; x < 3; x++)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        grid[index.X * 3 + x, index.Y * 3 + y] = original[index];
+                    }
+                }
+            }
+
+            var seen = new HashSet<Vector>();
+
+            var total = 0;
+
+            foreach (var index in grid.Indexes())
+            {
+                if (seen.Contains(index))
+                {
+                    continue;
+                }
+
+                var plot = new HashSet<Vector>();
+                Utils.FloodFill(index, (n, _) =>
+                {
+                    if (!seen.Add(n))
+                    {
+                        return Enumerable.Empty<Vector>();
+                    }
+
+                    plot.Add(n);
+                    return grid.Neighbors(n, false).Where(x => grid[x] == grid[n]);
+                });
+
+                var sides = plot.Count(x =>
+                {
+                    var connected = x.Neighbors(false).Count(plot.Contains);
+                    var empty = x.Neighbors(true).Count(y => !plot.Contains(y));
+                    return connected == 2 || (connected == 4 && empty == 1);
+                });
+
+                total += (plot.Count / 9) * sides;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Aoc/Aoc/y2024/Day13.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Aoc.y2024
+{
+    public class Day13 : DayBase
+    {
+        public Day13() : base(13)
+        {
+        }
+
+        public override void Solve()
+        {
+            this.SolveImpl(false);
+        }
+
+        private void SolveImpl(bool isMain)
+        {
+            var l = this.GetInputLines().ToList();
+
+            var re = new Regex(@"(\d+),[^\d]*(\d+)");
+            var total = 0L;
+            for (var i = 0; i < l.Count; i += 4)
+            {
+                var (ax, ay) = Parse(l[i]);
+                var (bx, by) = Parse(l[i + 1]);
+                var (tx, ty) = Parse(l[i + 2]);
+
+                if (isMain)
+                {
+                    tx += 10000000000000L;
+                    ty += 10000000000000L;
+                }
+
+                // tx = a*ax + b*bx
+                // ty = a*ay + b*by
+                // a = (tx - b*bx)/ax
+                // ty = ay*(tx - b*bx)/ax + b*by
+                // ty*ax = ay*tx - b*bx*ay + b*by*ax
+                // ty*ax - ay*tx = b*(by*ax - bx*ay)
+                // b = (ty*ax - ay*tx)/(by*ax - bx*ay)
+
+                var num = ty * ax - ay * tx;
+                var denom = by * ax - bx * ay;
+                if (num % denom != 0)
+                {
+                    continue;
+                }
+
+                var b = num / denom;
+                num = tx - b * bx;
+                if (num % ax != 0)
+                {
+                    continue;
+                }
+                var a = num / ax;
+                if (!isMain && (a > 100 || b > 100))
+                {
+                    continue;
+                }
+                total += 3 * a + b;
+            }
+
+            Console.WriteLine(total);
+
+            (long, long) Parse(string line)
+            {
+                var match = re.Match(line);
+                return (long.Parse(match.Groups[1].Value), long.Parse(match.Groups[2].Value));
+            }
+        }
+
+        public override void SolveMain()
+        {
+            SolveImpl(true);
+        }
+    }
+}
